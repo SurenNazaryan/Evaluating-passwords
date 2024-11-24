c32d8c10940140777783d52bae3abbbadd0b5db6
SurenNazaryan <127138768+SurenNazaryan@users.noreply.github.com>
2024-11-24 19:51:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import urwid
+
+
+conditions = []
+
+
+def is_very_long(password):
+    return len(password) > 12
+
+
+conditions.append(is_very_long)
+
+
+def has_digit(password):
+    return any(char.isdigit() for char in password)
+
+
+conditions.append(has_digit)
+
+
+def has_letters(password):
+    return any(char.isalpha() for char in password)
+
+
+conditions.append(has_letters)
+
+
+def has_upper_letters(password):
+    return any(char.isupper() for char in password)
+
+
+conditions.append(has_upper_letters)
+
+
+def has_lower_letters(password):
+    return any(char.islower() for char in password)
+
+
+conditions.append(has_lower_letters)
+
+
+def has_symbols(password):
+    return any(i in '%#' for i in password)
+
+
+conditions.append(has_symbols)
+
+
+def on_ask_change(edit, new_edit_text):
+    rating = 0
+    for i in conditions:
+        if i(new_edit_text):
+            rating += 2
+    reply.set_text("Рейтинг: %s" % rating)
+
+
+if __name__ == '__main__':
+    ask = urwid.Edit('Тайный ввод: ', mask='*')
+    reply = urwid.Text("")
+    menu = urwid.Pile([ask, reply])
+    menu = urwid.Filler(menu, valign='top')
+    urwid.connect_signal(ask, 'change', on_ask_change)
+    urwid.MainLoop(menu).run()
