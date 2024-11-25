e1380d6a7782d44a32667a91d4500baf9a58034b
SurenNazaryan <127138768+SurenNazaryan@users.noreply.github.com>
2024-11-25 12:48:00 +0300

    Исправил ошибки

--- main.py
+++ main.py
@@ -1,53 +1,40 @@
 import urwid
 
 
-conditions = []
-
-
-def is_very_long(password):
-    return len(password) > 12
-
-
-conditions.append(is_very_long)
-
-
-def has_digit(password):
-    return any(char.isdigit() for char in password)
-
-
-conditions.append(has_digit)
-
-
-def has_letters(password):
-    return any(char.isalpha() for char in password)
-
-
-conditions.append(has_letters)
-
-
-def has_upper_letters(password):
-    return any(char.isupper() for char in password)
-
-
-conditions.append(has_upper_letters)
-
-
-def has_lower_letters(password):
-    return any(char.islower() for char in password)
-
-
-conditions.append(has_lower_letters)
-
-
-def has_symbols(password):
-    return any(i in '%#' for i in password)
-
-
-conditions.append(has_symbols)
-
-
 def on_ask_change(edit, new_edit_text):
     rating = 0
+    conditions = []
+
+    def is_very_long(password):
+        return len(password) > 12
+
+    conditions.append(is_very_long)
+
+    def has_digit(password):
+        return any(char.isdigit() for char in password)
+
+    conditions.append(has_digit)
+
+    def has_letters(password):
+        return any(char.isalpha() for char in password)
+
+    conditions.append(has_letters)
+
+    def has_upper_letters(password):
+        return any(char.isupper() for char in password)
+
+    conditions.append(has_upper_letters)
+
+    def has_lower_letters(password):
+        return any(char.islower() for char in password)
+
+    conditions.append(has_lower_letters)
+
+    def has_symbols(password):
+        return any(not i.isalnum() for i in password)
+
+    conditions.append(has_symbols)
+
     for i in conditions:
         if i(new_edit_text):
             rating += 2
